0112341ecd7bb86684b8daad9aa80e690010599d
“ElvinShalmiev” <elvin080192@gmail.com.com>
2023-02-04 14:52:04 +0200

    updated/responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,11 @@
 
 
-/* ====2 version  */
 
- import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import BookList from './BookList';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
-const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const navigate = useNavigate();
-
-  return (
-    <div className='navbar'>
-      <div>
-        <Link to="/">
-          <h1 onClick={() => navigate(`/books/${BookList}`)}>BookHub</h1>
-        </Link>
-      </div>
-
-      
-      <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <Link to="/about">
-          <h3>About</h3>
-        </Link>
-        
-        <Link to="/favorites">
-          <h3>Your Favorites</h3>
-        </Link>
-      </div>
-
-      <div className="hamburger">
-          <FontAwesomeIcon onClick={() => setMenuOpen(!menuOpen)} icon={faBars} />
-      </div>
-
-    </div>
-  );
-};
-
-export default Navbar;  
-
-
-
-
-/* ===1 version ==================== */
-
-/* import React from 'react'
+import React from 'react'
 import '../App';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import BookList from './BookList';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
 
@@ -64,11 +16,6 @@ const Navbar = () => {
                 <Link to="/">
                 <h1 onClick={() => navigate(`/books/${BookList}`)}>BookHub</h1>
                 </Link>
-            </div>
-            <div>
-                <div className="hamburger">
-                <FontAwesomeIcon icon={faBars} />
-            </div>
             </div>
             <div>
                 <Link to="/about">
@@ -84,4 +31,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar */
+export default Navbar 
